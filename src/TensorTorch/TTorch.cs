e2cc78dc85a49fd7d0faf3e31fb038c1b443a4f6
zhouyuelin <as.zhouyuelin@aisino.com>
2025-02-16 15:42:53 +0800

    Add project: TensorTorch

---
+++ src/TensorTorch/TTorch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zyl.TensorTorch {
+    /// <summary>
+    /// Tensor Torch util (张量 Torch 工具).
+    /// </summary>
+    public static class TTorch {
+    }
+}
