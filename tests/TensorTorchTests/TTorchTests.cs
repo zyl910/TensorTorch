088e642f316c22ba8458495aa9b42493b6d7e222
zhouyuelin <as.zhouyuelin@aisino.com>
2025-02-23 17:51:49 +0800

    Add TensorTorchTests.csproj

---
+++ tests/TensorTorchTests/TTorchTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Zyl.TensorTorch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zyl.TensorTorch.Tests {
+    [TestFixture()]
+    public class TTorchTests {
+        [Test()]
+        public void ArangeTest() {
+            Assert.Fail();
+        }
+    }
+}
