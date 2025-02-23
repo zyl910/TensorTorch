009331c658969872fbb86dc87cba94bd9215fdf7
zhouyuelin <as.zhouyuelin@aisino.com>
2025-02-23 20:37:01 +0800

    Config TargetFramework

--- samples/SampleD2L/Program.cs
+++ samples/SampleD2L/Program.cs
@@ -4,6 +4,12 @@ namespace Zyl.SampleD2L {
         static void Main(string[] args) {
             TextWriter writer = Console.Out;
             writer.WriteLine("SampleD2L");
+            writer.WriteLine(string.Format("RuntimeInformation.FrameworkDescription:\t{0}", System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription));
+            writer.WriteLine(string.Format("RuntimeInformation.OSArchitecture:\t{0}", System.Runtime.InteropServices.RuntimeInformation.OSArchitecture));
+            writer.WriteLine(string.Format("RuntimeInformation.OSDescription:\t{0}", System.Runtime.InteropServices.RuntimeInformation.OSDescription)); // Same Environment.OSVersion. It's more accurate.
+            writer.WriteLine(string.Format("RuntimeInformation.RuntimeIdentifier:\t{0}", System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier)); // e.g. win10-x64
+            writer.WriteLine();
+
             D2LMain.Output(writer);
         }
     }
